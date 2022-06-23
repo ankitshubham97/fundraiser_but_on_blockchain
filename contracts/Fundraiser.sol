d2d3eb4d275167d24defdc788069ea038bbe6fc5
Ankit Shubham <ankitshubham97@gmail.com>
2022-06-23 22:05:24 +0000

    fix: lower the minimum funding amount

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -20,11 +20,8 @@ contract Fundraiser {
     }
 
     function fund() public payable {
-        uint256 minimumUSD = 50 * 10**18;
-        require(
-            getConversionRate(msg.value) >= minimumUSD,
-            "You need to spend more ETH!"
-        );
+        uint256 minimumUSD = 5 * 10**18;
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
